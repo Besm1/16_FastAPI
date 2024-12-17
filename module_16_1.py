ce0941b7058e46a9db5e3e5ef24d746b2d7fbdeb
besm1 <besn1@yandex.ru>
2024-12-17 17:50:42 +0300

    Module_16_1 Correction

--- module_16_1.py
+++ module_16_1.py
@@ -4,19 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 async def get_main():
-    return {'Главная страница'}
+    return 'Главная страница'
 
 @app.get("/user/admin")
-async def get_main():
-    return {'Вы вошли как администратор'}
+async def get_adm():
+    return 'Вы вошли как администратор'
 
 @app.get("/user/{user_id}")
-async def get_main(user_id:int):
-    return {f'Вы вошли как пользователь № {user_id}'}
+async def get_user_by_id(user_id:int):
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get("/user")
-async def get_main(username:str, age:int):
-    return {f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
-
-
-
+async def get_user_by_name(username:str, age:int):
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
