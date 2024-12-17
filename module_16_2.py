88e0082f12df81116bc2f5b708407f5c4cfba625
besm1 <besn1@yandex.ru>
2024-12-17 21:44:31 +0300

    Module_16_2

--- module_16_2.py
+++ module_16_2.py
@@ -12,6 +12,7 @@ async def get_main():
 async def get_user_by_id(user_id:Annotated[int, Path(..., ge=1, le=100, description='Enter User ID')]):
     return f'Вы вошли как пользователь № {user_id}'
 
-@app.get("/user")
-async def get_user_by_name(username:str, age:int):
+@app.get("/user/{username}/{age}")
+async def get_user_by_name(username:Annotated[str, Path(..., min_length=5, max_length=20)]
+                           , age:Annotated[int, Path(..., ge=18, le=120)]):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
