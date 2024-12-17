0396d3d74ee3bfea09a560035e7c7449594f8743
besm1 <besn1@yandex.ru>
2024-12-17 21:37:26 +0300

    Module_16_1 Correction

--- module_16_2.py
+++ module_16_2.py
@@ -1,19 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
+
 
 @app.get("/")
 async def get_main():
     return 'Главная страница'
 
-@app.get("/user/admin")
-async def get_adm():
-    return 'Вы вошли как администратор'
-
 @app.get("/user/{user_id}")
-async def get_user_by_id(user_id:int):
+async def get_user_by_id(user_id:Annotated[int, Path(..., ge=1, le=100, description='Enter User ID')]):
     return f'Вы вошли как пользователь № {user_id}'
 
-@app.get("/user/{username}/{age}")
+@app.get("/user")
 async def get_user_by_name(username:str, age:int):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
